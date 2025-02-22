d85651e9792881004740c40c2cc69bf505f18ba0
Faldiansyah <faldiansyahk@gmail.com>
2025-02-23 03:21:10 +0700

    receive data ajax

--- ajax.js
+++ ajax.js
@@ -1,3 +1,12 @@
+const response = document.createElement('h1');
+response.id = 'response';
+document.body.appendChild(response);
+
 const ajax = new XMLHttpRequest();
 ajax.open('GET', 'api/data.json');
+ajax.addEventListener('load', function () {
+    const json = JSON.parse(ajax.responseText);
+    const header = document.getElementById('response');
+    header.textContent = json.response;
+});
 ajax.send();
